71cf9e67a0637b58dc3706ec5f3ea1d2000e9672
caique-az <caiqueazevedo2005@gmail.com>
2026-04-28 11:03:43 -0300

    chore: remove forceExit from jest config

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   testTimeout: 30000,
   verbose: true,
-  forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
