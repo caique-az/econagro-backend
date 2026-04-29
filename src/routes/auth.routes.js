dd4b1d4986fa325827914f4351bb4c556e800b03
caique-az <caiqueazevedo2005@gmail.com>
2026-04-29 15:04:49 -0300

    feat: add forgot-password and reset-password routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,5 +7,7 @@ const { authenticate } = require('../middlewares/auth');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/me', authenticate, authController.me);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
